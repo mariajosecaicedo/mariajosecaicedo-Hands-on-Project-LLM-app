c740b5fb7c8c96d7e7ecb2884d207046e0d11ca9
mariajosecaicedo <mayita.caicedo2000@gmail.com>
2026-04-25 11:35:19 -0500

    Add Streamlit app for generating responses with OpenAI

---
+++ streamlit_app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from langchain_openai import OpenAI
+
+st.title('🦜🔗 Quickstart App')
+
+openai_api_key = st.secrets["OPENAI_API_KEY"]
+
+def generate_response(input_text):
+    llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
+    st.info(llm.invoke(input_text))
+
+with st.form('my_form'):
+    text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
+    submitted = st.form_submit_button('Submit')
+    if submitted:
+        generate_response(text)
